1b7de2a13d5475fd8c32540fd83f5685423cc53e
again <cui.zhengze@huidatech.cn>
2020-10-20 16:10:31 +0800

    bug fix

--- src/emqx_bridge_kafka.erl
+++ src/emqx_bridge_kafka.erl
@@ -68,7 +68,7 @@ load(Env) ->
     % emqx:hook('session.discarded',   {?MODULE, on_session_discarded, [Env]}),
     % emqx:hook('session.takeovered',  {?MODULE, on_session_takeovered, [Env]}),
     % emqx:hook('session.terminated',  {?MODULE, on_session_terminated, [Env]}),
-    emqx:hook('message.publish',     {?MODULE, on_message_publish, [Env]}),
+    emqx:hook('message.publish',     {?MODULE, on_message_publish, [Env]}).
     % emqx:hook('message.delivered',   {?MODULE, on_message_delivered, [Env]}),
     % emqx:hook('message.acked',       {?MODULE, on_message_acked, [Env]}),
     % emqx:hook('message.dropped',     {?MODULE, on_message_dropped, [Env]}).
@@ -243,7 +243,7 @@ unload() ->
     % emqx:unhook('session.discarded',   {?MODULE, on_session_discarded}),
     % emqx:unhook('session.takeovered',  {?MODULE, on_session_takeovered}),
     % emqx:unhook('session.terminated',  {?MODULE, on_session_terminated}),
-    emqx:unhook('message.publish',     {?MODULE, on_message_publish}),
+    emqx:unhook('message.publish',     {?MODULE, on_message_publish}).
     % emqx:unhook('message.delivered',   {?MODULE, on_message_delivered}),
     % emqx:unhook('message.acked',       {?MODULE, on_message_acked}),
     % emqx:unhook('message.dropped',     {?MODULE, on_message_dropped}).
