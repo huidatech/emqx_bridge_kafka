733fc182a93c8ce6aebe640d851c0a04428fe2b3
again <cui.zhengze@huidatech.cn>
2021-01-23 09:18:51 +0800

    modify produce

--- src/emqx_bridge_kafka.erl
+++ src/emqx_bridge_kafka.erl
@@ -50,6 +50,23 @@
         , on_message_dropped/4
         ]).
 
+-define(WAIT(Pattern, Handle, Timeout),
+        fun() ->
+          receive
+            Pattern ->
+              Handle;
+            Msg ->
+              erlang:error({unexpected,
+                            [{line, ?LINE},
+                             {pattern, ??Pattern},
+                             {received, Msg}]})
+          after
+            Timeout ->
+              erlang:error({timeout,
+                            [{line, ?LINE},
+                             {pattern, ??Pattern}]})
+          end
+        end()).
 %% Called when the plugin application start
 load(Env) ->
     brod_init([Env]),
@@ -228,7 +245,10 @@ on_message_publish(Message, _Env) ->
     PartitionFun = fun(_Topic, PartitionsCount, _Key, _Value) ->
                 {ok, crypto:rand_uniform(0, PartitionsCount)}
                 end,
-    brod:produce(brod_client_1, ProduceTopic, PartitionFun, <<>>, Json),
+    {ok, CallRef} = brod:produce(brod_client_1, ProduceTopic, PartitionFun, <<>>, Json),
+    ?WAIT(#brod_produce_reply{call_ref = CallRef,
+                            result = brod_produce_req_acked},
+        ok, 2000),
     % ekaf:produce_async(ProduceTopic, Json),
     % ekaf:produce_async(Topic, Payload),
     {ok, Message}.
